6c313fe4c65af4789a96ebe4c2423c523ec69a9b
lsahi <30521187+lsahi@users.noreply.github.com>
2018-04-15 11:47:43 +0800

    connected to database

--- src/com/util/C3P0.java
+++ src/com/util/C3P0.java
@@ -10,31 +10,13 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class C3P0 {
 	//
-	/**数据库连接池
-	 * URL=数据库位置
-	 * USER=
-	 * PASSWORD=
-	 * DRIVERCLASS=
-	 * sql=默认语句->查询整个表
-	*/
-	private static final String URL = "jdbc:mysql://localhost/student";
+
+	private static final String URL = "jdbc:mysql://localhost/androidserver?characterEncoding=utf8&useSSL=false";
 	private static final String USER = "root";
 	private static final String PASSWORD = "123456";
 	private static final String DRIVERCLASS = "com.mysql.jdbc.Driver";
 	private static final String sql="select * from student";
 	static ComboPooledDataSource dataSource = new ComboPooledDataSource();
-	
-	/*
-	用于连接本机上的默认实例名  
-  
-  String URL ="jdbc:sqlserver://localhost;integratedSecurity=true;DatabaseName=JspDB";  
-  
-  //用于连接本机上的指定实例名(如果有两个实例,这时的端口号默认为是:1434)  
-  
-  //要先开启SQL Server服务/SQL Server Browser这项服务  
-  
-  String url = "jdbc:sqlserver://localhost;instanceName=MYSQL2008;integratedSecurity=true;DatabaseName=jdbcDB";    
-	 * */
 	
 	static {
 		dataSource.setUser(USER);
